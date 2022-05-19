0db9cfc2888e6b75fdbc120b797f2cb7a51984e2
subhamghimire <subham.ghimire26@gmail.com>
2022-05-19 10:19:08 +0545

    Sidebar added

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import { observer, useLocalObservable } from 'mobx-react-lite'
+import styles from './Menu.module.scss'
+import { Link } from 'react-router-dom'
+
+export default observer(() => {
+  const store = useLocalObservable(() => ({
+    showMenu: false,
+    storeMenu: false,
+  }))
+  return (
+    <div className="p-6">
+      <div className="flex flex-col gap-4">
+        <div
+          className={styles.relativeContainer}
+          onMouseEnter={() => {
+            store.storeMenu = !store.storeMenu
+          }}
+        >
+          <Link to="/store" className={styles.footerLink}>
+            STORE
+          </Link>
+          {store.storeMenu && (
+            <div
+              className={styles.menuContainer}
+              onMouseEnter={() => (store.storeMenu = true)}
+              onMouseLeave={() => (store.storeMenu = false)}
+            >
+              <div>
+                <Link to="/store/tshirts" className={styles.storeMenuLink}>
+                  T-SHIRTS
+                </Link>
+              </div>
+              <div>
+                <Link to="/store/sweatshirts" className={styles.storeMenuLink}>
+                  SWEATSHIRTS
+                </Link>
+              </div>
+              <div>
+                <Link to="/store/jackets" className={styles.storeMenuLink}>
+                  JACKETS
+                </Link>
+              </div>
+              <div>
+                <Link to="/store/bottoms" className={styles.storeMenuLink}>
+                  BOTTOMS
+                </Link>
+              </div>
+              <div>
+                <Link to="/store/caps" className={styles.storeMenuLink}>
+                  CAPS
+                </Link>
+              </div>
+              <div>
+                <Link to="/store/accessories" className={styles.storeMenuLink}>
+                  ACCESSORIES
+                </Link>
+              </div>
+            </div>
+          )}
+        </div>
+        <Link className={styles.footerLink} to="/about-us">
+          ABOUT
+        </Link>
+        <Link className={styles.footerLink} to="/stocklist">
+          STOCKIST
+        </Link>
+        <Link className={styles.footerLink} to="/news">
+          NEWS
+        </Link>
+      </div>
+    </div>
+  )
+})
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,8 +1,23 @@
+import Logo from 'components/logo/Logo'
+import Menu from 'components/Menu/Menu'
+import { useEffect, useState } from 'react'
+import styles from './Sidebar.module.scss'
+import cls from 'classnames'
+
 export default function Sidebar() {
+  const [showMenu, setMenu] = useState(false)
+
   return (
-    <div className="sidebar bg-white shadow-sidebar-shadow absolute h-screen inset-y-0 left-0  -translate-x-0 transform transition duration-200 ease-in-out">
-      <div class="border border-blue-blue border-opacity-20 my-6 text-lg">
-        I am Sidebar
+    <div>
+      <div className="sidebar fixed h-screen inset-y-0 left-0 ">
+        <div class="my-6">
+          <div onClick={() => setMenu(true)}>
+            <div className={cls({ [styles.disabledEvents]: !showMenu })}>
+              <Logo size={80} className={styles.logo} />
+            </div>
+          </div>
+          <div>{showMenu ? <Menu /> : ''}</div>
+        </div>
       </div>
     </div>
   )
